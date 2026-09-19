cdcf4f879671e86b8378d4caa30b9b5bd5ce2d16
Srishti Srivastava <srishti.school2010@gmail.com>
2026-09-19 19:05:53 +0530

    feat: Created Hero section..
    -Added hero.jsx and hero.css.. with some text and button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import bg from "./assets/main.mp4";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <div className="App">
       <video src={bg} autoPlay muted loop className="video-bg"></video>
       <Navbar />
+      <Hero />
     </div>
   );
 }
